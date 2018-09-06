50e16721386b7ff93deb5e8fc4ba674424ba1a19
john <1351009316@qq.com>
2018-09-06 09:53:44 +0800

    增加参数说明

--- src/utils/getDom.js
+++ src/utils/getDom.js
@@ -1,4 +1,8 @@
 // 获取节点
+/**
+ * @param  {[String]}  dom   [节点的类名或Id]
+ * @return {[type]}          [description]
+ */
 function _getDomRect(dom) {
   return new Promise((resolve, reject) => {
     wx.createSelectorQuery().select(dom).boundingClientRect((res) => {
